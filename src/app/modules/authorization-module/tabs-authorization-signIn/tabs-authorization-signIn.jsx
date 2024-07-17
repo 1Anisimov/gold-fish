ba34107d7df52fcae005ec40a5f040a769935084
Alexandr Anisimov <anisimov2002kurgan@mail.ru>
2024-07-17 17:55:59 +0500

    Validator

--- src/app/modules/authorization-module/tabs-authorization-signIn/tabs-authorization-signIn.jsx
+++ src/app/modules/authorization-module/tabs-authorization-signIn/tabs-authorization-signIn.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import cls from './tabs-authorization-signIn.module.css';
 import MainButton from '../../../reusable-components/main-button';
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../../store/currentUser';
 import { setModalRegisterForm } from '../../../store/modals';
+import { validator } from '../../../utils/validator';
 
 const TabsAuthorizationSignIn = () => {
   const dispatch = useDispatch();
-  const [loginForm, setLoginForm] = useState({});
+  const [loginForm, setLoginForm] = useState({
+    email: '',
+    password: ''
+  });
+  const [errors, setErrors] = useState({});
+
   const handleChangeLoginForm = ({ target }) => {
     setLoginForm((prevState) => ({
       ...prevState,
@@ -19,6 +25,33 @@ const TabsAuthorizationSignIn = () => {
     dispatch(logIn({ payload: loginForm }));
     dispatch(setModalRegisterForm(false));
   };
+
+  const validatorConfig = {
+    email: {
+      isRequired: {
+        message: 'Электронная почта обязательна для заполнения'
+      },
+      isEmail: {
+        message: 'Некорректный адресс email'
+      }
+    },
+    password: {
+      isRequired: {
+        message: 'Пароль обязателен для заполнения'
+      }
+    }
+  };
+
+  useEffect(() => {
+    validate();
+  }, [loginForm]);
+
+  const validate = () => {
+    const allErrors = validator(loginForm, validatorConfig);
+    setErrors(allErrors);
+    console.log(errors);
+    return Object.keys(allErrors).length === 0;
+  };
   return (
     <div className={cls.signIn}>
       <div className={cls.contant}>
@@ -27,24 +60,33 @@ const TabsAuthorizationSignIn = () => {
             <label htmlFor="email">E-mail</label>
             <input
               onChange={handleChangeLoginForm}
-              className={cls.emailInput}
+              className={!errors.email ? cls.emailInput : cls.errorInput}
               type="email"
               name="email"
               placeholder="E-mail"
             />
+            {errors.email ? <div className={cls.errorBlock}>{errors.email}</div> : <></>}
           </div>
           <div className={cls.email}>
             <label htmlFor="password">Пароль</label>
             <input
               placeholder="Пароль"
               onChange={handleChangeLoginForm}
-              className={cls.emailInput}
+              className={!errors.password ? cls.emailInput : cls.errorInput}
               type="password"
               name="password"
             />
+            {errors.password ? <div className={cls.errorBlock}>{errors.password}</div> : <></>}
           </div>
           <p className={cls.p}>Забыли пароль?</p>
-          <MainButton handleClick={pushLoginForm} width="274" heigth="39" isGradient text="Войти" />
+          <MainButton
+            isDisabled={errors.email || errors.password ? true : false}
+            handleClick={pushLoginForm}
+            width="274"
+            heigth="39"
+            isGradient
+            text="Войти"
+          />
         </form>
       </div>
     </div>
